c5d76df905803c79d4480440accc33e79e8ce1b0
Vladimir Goldobin <vgold@xiag.ch>
2012-11-23 18:05:59 +0700

    publish demo

---
+++ web/index.php
@@ -0,0 +1,19 @@
+<?php
+require_once(__DIR__ . '/../vendor/autoload.php');
+$streamName = basename($_SERVER['REQUEST_URI']);
+$publisher = new \Webcam\Publisher('/tmp/av/');
+$filePath = $publisher->getLastFile($streamName);
+if ($filePath) {
+    $indexedFilePath = $publisher->indexFlv($filePath);
+    if ($indexedFilePath && file_exists($indexedFilePath)) {
+        $uploader = new \Webcam\BarberryUploader('http://barberry.local/');
+        header("Content-type: text/json", null, 200);
+        echo json_encode($uploader->upload($indexedFilePath));
+        exit(0);
+    }
+    header("Content-type: text/json", null, 500);
+    echo json_encode('unable to create index');
+    exit(1);
+}
+header("Content-type: text/json", null, 404);
+echo json_encode('file not found');
